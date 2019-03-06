5faed1844a7e505dcc7b0019db1c11f246c90ad3
Lauren <laurenqurashi@me.com>
2019-03-06 16:49:19 +0000

    Completed Step 11 - accessing bike rack to enable bike docking to happen

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,22 @@
+
+
 class DockingStation
+attr_reader :bike_rack
+  def initialize
+    @bike_rack = []
+  end
+
   def release_bike
     bike = Bike.new
+  end
+
+  def dock_bike(bike)
+    @bike_rack << bike
+  end
+
+  def full?
+  end
+
+  def empty?
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,4 +10,16 @@ describe DockingStation do
     expect(bike).to respond_to(:working?)
   end
 
+  it { is_expected.to respond_to(:dock_bike).with(1).argument }
+
+  it 'adds a bike to the docking station when we call dock_bike' do
+    docking_station = DockingStation.new
+    bike = docking_station.release_bike
+    docking_station.dock_bike(bike)
+    expect(docking_station.bike_rack).to eq [bike]
+  end
+
+  #it { is_expected.to respond_to(:full?) }
+
+  #it { is_expected.to respond_to(:empty?) }
 end
