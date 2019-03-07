b065e5a3e0c509612f68c389b0dc7339f4f7277d
Lauren <laurenqurashi@me.com>
2019-03-07 15:48:53 +0000

    Step 14 completed, refactored for SRP

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,18 +7,22 @@ attr_reader :bike_rack
   end
 
   def release_bike
-    raise("No bikes!") if @bike_rack.empty?
+    raise("No bikes!") if empty?
     bike = Bike.new
   end
 
   def dock_bike(bike)
-    raise("Bike Rack is full!") if @bike_rack.length >= 20
+    raise("Bike Rack is full!") if full?
     @bike_rack << bike
   end
 
+  private
+
   def full?
+    @bike_rack.length >= 20
   end
 
   def empty?
+    @bike_rack.empty?
   end
 end
