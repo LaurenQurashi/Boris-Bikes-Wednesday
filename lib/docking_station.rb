1fe5cd0e9f896b99e0cfed141a1bcd5ca376ea4d
Lauren <laurenqurashi@me.com>
2019-03-07 15:15:49 +0000

    Step 13 completed. Enabled a feature that prevents bike docking in a full bike rack, includes refactoring.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -12,6 +12,7 @@ attr_reader :bike_rack
   end
 
   def dock_bike(bike)
+    raise("Bike Rack is full!") if @bike_rack.length == 1
     @bike_rack << bike
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,32 +2,34 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
+  before(:each) do
+    @bike1 = Bike.new
+    @docking_station = DockingStation.new
+  end
+
   it { is_expected.to respond_to(:release_bike) }
 
   it 'returns a bike when we call release bike' do
-    docking_station = DockingStation.new
-    bike = Bike.new
-    docking_station.dock_bike(bike)
-    expect(docking_station.release_bike).to be_a(Bike)
-    bike = docking_station.release_bike
-    expect(bike).to respond_to(:working?)
+    @docking_station.dock_bike(@bike1)
+    expect(@docking_station.release_bike).to be_a(Bike)
+    expect(@bike1).to respond_to(:working?)
   end
 
   it { is_expected.to respond_to(:dock_bike).with(1).argument }
 
   it 'adds a bike to the docking station when we call dock_bike' do
-    docking_station = DockingStation.new
-    bike = Bike.new
-    docking_station.dock_bike(bike)
-    expect(docking_station.bike_rack).to eq [bike]
+    @docking_station.dock_bike(@bike1)
+    expect(@docking_station.bike_rack).to eq [@bike1]
   end
 
   it 'raises an error if there are no bikes to release' do
-    docking_station = DockingStation.new
-    expect { docking_station.release_bike }.to raise_error("No bikes!")
+    expect { @docking_station.release_bike }.to raise_error("No bikes!")
   end
 
-  #it { is_expected.to respond_to(:full?) }
+  it 'raises an error if the bike_rack is full' do
+    bike2 = Bike.new
+    @docking_station.dock_bike(@bike1)
+    expect { @docking_station.dock_bike(bike2) }.to raise_error("Bike Rack is full!")
+  end
 
-  #it { is_expected.to respond_to(:empty?) }
 end
